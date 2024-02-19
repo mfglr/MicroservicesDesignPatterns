060b171a1e358012b65e6a25ab97216553e21e23
mfglr <mfgglr@outlook.com>
2024-02-20 02:27:13 +0300

    SagaOrchestration step4

---
+++ SageStateMachine.WorkerService/Models/OrderStateDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+
+namespace SageStateMachine.WorkerService.Models
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations => new List<ISagaClassMap>() { new OrderStateMap() };
+    }
+}
---
+++ SageStateMachine.WorkerService/Models/OrderStateMap.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SageStateMachine.WorkerService.Models
+{
+    public class OrderStateMap : SagaClassMap<OrderStateInstance>
+    {
+        protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+        {
+            base.Configure(entity, model);
+        }
+    }
+}
